1be575ab30d10fb563316171ef06e4c6d26f0a42
AfanasevStas <afanasevstas96@mail.ru>
2026-01-29 19:50:54 +0300

    HideControls in Progress

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -34,6 +34,7 @@
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
             this.checkBoxShowWeekday = new System.Windows.Forms.CheckBox();
+            this.buttonHideControls = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelTime
@@ -66,18 +67,30 @@
             // 
             this.checkBoxShowWeekday.AutoSize = true;
             this.checkBoxShowWeekday.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBoxShowWeekday.Location = new System.Drawing.Point(22, 230);
+            this.checkBoxShowWeekday.Location = new System.Drawing.Point(22, 208);
             this.checkBoxShowWeekday.Name = "checkBoxShowWeekday";
             this.checkBoxShowWeekday.Size = new System.Drawing.Size(180, 29);
             this.checkBoxShowWeekday.TabIndex = 2;
             this.checkBoxShowWeekday.Text = "Show Weekday";
             this.checkBoxShowWeekday.UseVisualStyleBackColor = true;
             // 
+            // buttonHideControls
+            // 
+            this.buttonHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHideControls.Location = new System.Drawing.Point(22, 254);
+            this.buttonHideControls.Name = "buttonHideControls";
+            this.buttonHideControls.Size = new System.Drawing.Size(274, 45);
+            this.buttonHideControls.TabIndex = 3;
+            this.buttonHideControls.Text = "Hide controls";
+            this.buttonHideControls.UseVisualStyleBackColor = true;
+            this.buttonHideControls.Click += new System.EventHandler(this.buttonHideControls_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(324, 311);
+            this.Controls.Add(this.buttonHideControls);
             this.Controls.Add(this.checkBoxShowWeekday);
             this.Controls.Add(this.checkBoxShowDate);
             this.Controls.Add(this.labelTime);
@@ -97,6 +110,7 @@
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.CheckBox checkBoxShowDate;
         private System.Windows.Forms.CheckBox checkBoxShowWeekday;
+        private System.Windows.Forms.Button buttonHideControls;
     }
 }
 
